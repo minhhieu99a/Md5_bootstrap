3d454e79375e36ee6ced675b6b2889bd81ec6477
minhhieu99a <hieunb99@gmail.com>
2022-05-24 10:37:31 +0700

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Product3Component } from './product3/product3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Product3Component
   ],
   imports: [
     BrowserModule,
